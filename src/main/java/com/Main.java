c06781d604f579f57c829b8863119f0f272949dd
Kirill1503 <105604464+Kirill1503@users.noreply.github.com>
2025-05-25 00:28:01 +0300

    First commit

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,17 +1,27 @@
 package com;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.geometry.Circle;
+import com.geometry.Rectangle;
+import com.geometry.Triangle;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Создаём фигуры
+        Circle circle = new Circle(5);
+        Rectangle rectangle = new Rectangle(4, 6);
+        Triangle triangle = new Triangle(3, 4, 5, 4);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Выводим площади и периметры
+        System.out.println("=== Circle ===");
+        System.out.println("Area: " + circle.getArea());
+        System.out.println("Perimeter: " + circle.getPerimeter());
+
+        System.out.println("\n=== Rectangle ===");
+        System.out.println("Area: " + rectangle.getArea());
+        System.out.println("Perimeter: " + rectangle.getPerimeter());
+
+        System.out.println("\n=== Triangle ===");
+        System.out.println("Area: " + triangle.getArea(3, 4));
+        System.out.println("Perimeter: " + triangle.getPerimeter());
     }
 }
